804772446e352bbda3fd839fa240124fcd7940ce
Corentin Pla <103648354+corentinpla@users.noreply.github.com>
2022-12-21 20:48:32 +0100

    eejfdzfzefzefzef@@;:@@@#:

--- glissement_terrain.py
+++ glissement_terrain.py
@@ -1,20 +1,21 @@
-from math import *
+import math 
 import pandas as pd 
+
 
 #Calcule la distance entre deux point repérés par leur latitude et leur longitude
 def distance_lat_long(lat1, lat2, lon1, lon2):
      
-    lon1 = radians(lon1)
-    lon2 = radians(lon2)
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
+    lon1 = math.radians(lon1)
+    lon2 = math.radians(lon2)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
       
     # Formule de Haversine
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
  
-    c = 2 * asin(sqrt(a))
+    c = 2 * math.asin(math.sqrt(a))
     
     # rayon de la terre
     r = 6371
@@ -22,7 +23,7 @@ def distance_lat_long(lat1, lat2, lon1, lon2):
     return(c * r)
 
 #evenements à moins de 10km et datant de moins de 100 ans
-def event_less_10km(lat1, lon1, df): 
+def event_less_10km(lat1,lon1,df): 
                                 #lat1 lon1 : latitude, longitude de l'habitation considérée
  
   dico={"distance":[],"date":[]}
@@ -40,7 +41,7 @@ def event_less_10km(lat1, lon1, df):
   return(dico)
 
 
-def calcul_risque(dico, df): #dico : celui return par event_less_10km 
+def calcul_risque(dico): #dico : celui return par event_less_10km 
 
   risque=0
   quali_data=len(dico["date"]) #proportion d'event pour lasquelles on a la date/nb total d'event
@@ -54,12 +55,14 @@ def calcul_risque(dico, df): #dico : celui return par event_less_10km
 
     else : 
 
-      annee=int(df["dateDebut"][k][:4])
+      annee=int(dico["date"][k][:4])
                 
     if 1900<=annee<=2022:
 
       risque=risque+(10-dico["distance"][k])*annee
-  
-  quali_data=quali_data/len(dico["date"])
+  risque=(risque/2000000)*20 ##constante de normalisation trouvée en considérant le risque maximal observé
+
+  if len(dico["date"])!=0:
+    quali_data=quali_data/len(dico["date"])
     
   return (risque, quali_data)
